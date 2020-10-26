74f00e572d5e4e02a81dc4ebcd5c9585a5cee70b
Ge, Houbao <houge@paypal.com>
2020-10-26 20:56:25 +0800

    feature:「沉洋实验室」页面样式重设计

--- src/view/front/LabPage.vue
+++ src/view/front/LabPage.vue
@@ -1,8 +1,11 @@
 <template>
     <div class='full-width lab-page'>
 		<div class="lp-box">
-			<div class="item" v-for="(item, index) in sets" :key="index" @click="itemClick(item)" :title="item.name">
-				<i :class="item.icon"></i><span class="text-ellipsis">{{item.name}}</span>
+			<div  class="lp-item-box" v-for="(item, index) in sets" :key="index" @click="itemClick(item)">
+				<div class="lp-item-container" :title="item.name" :style="`background-image:url(${item.icon}; background-color: ${item.bgColor}`">
+					<div class="lp-ic-title">{{item.name}}</div>
+					<div class="lp-ic-intro">{{item.intro}}</div>
+				</div>
 			</div>
 		</div>
     </div>
@@ -19,75 +22,87 @@ export default {
 				{
 					id: 'theater',
 					name: '豆瓣影院',
+					intro: '倾心打造高品质、优体验、真良心的开放影院',
 					url: '/theater',
-					bgColor: '#BBD2F2',
-					icon: 'icon-ios-film'
+					bgColor: '#b8a6ff',
+					icon: 'https://gallery.sleepyocean.cn/resource/img/7a7d20fdbd1864358e878148789338c8'
 				}, {
 					id: 'TS',
 					name: '豆瓣图书',
+					intro: '高效查找你想看的图书',
 					url: '/book',
-					bgColor: '#E5D8EB',
-					icon: 'icon-ios-journal'
+					bgColor: '#e2d0ea',
+					icon: 'https://gallery.sleepyocean.cn/resource/img/bd78e8e4c3c82b35920b24557e2e0032'
 				}, {
 					id: 'TP',
 					name: '壁纸天堂',
+					intro: '沉洋精选壁纸合集',
 					url: '/photograph',
-					bgColor: '#BFB8BD',
-					icon: 'icon-ios-images'
+					bgColor: '#a84bd8',
+					icon: 'https://gallery.sleepyocean.cn/resource/img/c7e8852da88f97bf45144ffba706e8b3'
 				}, {
 					id: 'TL',
 					name: '时钟锁屏',
+					intro: '休息一会，让显示器做个安静的时钟吧',
 					url: '/time',
-					bgColor: '#ADA3BA',
-					icon: 'icon-ios-timer'
+					bgColor: '#353438',
+					icon: 'https://gallery.sleepyocean.cn/resource/img/274da3185361ff08040649bbfa39285d'
 				}, {
 					id: 'CSS',
 					name: 'CSS布局',
+					intro: '有趣的CSS实验室',
 					url: '/css-layout',
 					bgColor: '#B6ABB7',
-					icon: 'icon-ios-tablet-landscape'
+					icon: 'https://gallery.sleepyocean.cn/resource/img/1f4f976697adb764241cb92a5fb756f6'
 				}, {
 					id: '3D',
 					name: '3D实验室',
+					intro: '在这里，你就是上帝',
 					url: '/3D',
-					bgColor: '#AD8A93',
-					icon: 'icon-ios-cube'
+					bgColor: '#ff6a90',
+					icon: 'https://gallery.sleepyocean.cn/resource/img/65ba977962128fe5688a48b86de22279'
 				}, {
 					id: 'TEST',
 					name: 'IDEA实验室',
+					intro: '让梦想在这里实现吧',
 					url: '/idea-lab',
-					bgColor: '#976253',
-					icon: 'icon-ios-flask'
+					bgColor: '#b9ca31',
+					icon: 'https://gallery.sleepyocean.cn/resource/img/1eaf7929e2c23c3c75f6fa3e0298f661'
 				}, {
 					id: 'TYPING',
 					name: '沉洋打字机',
+					intro: '小键盘练习机，慧慧同学的练习场',
 					url: '/typing',
-					bgColor: '#7C4544',
-					icon: 'icon-ios-keypad'
+					bgColor: '#0bb127',
+					icon: 'https://gallery.sleepyocean.cn/resource/img/1048be1d9a30116adc253e6a1057e49b'
 				}, {
 					id: 'TYPINGCH',
 					name: '拼音打字机',
+					intro: '拼音练习机，慧慧同学的练习场',
 					url: '/typingChinese',
-					bgColor: '#62394A',
-					icon: 'icon-ios-barcode'
+					bgColor: '#d05656',
+					icon: 'https://gallery.sleepyocean.cn/resource/img/23a19ec65490204e4aa6b2ee21bf9494'
 				}, {
 					id: 'FILESYSTEM',
 					name: '云文件管理器',
+					intro: '在线文件管理器，搭建自己的百度云',
 					url: '/fileSystem',
-					bgColor: '#544667',
-					icon: 'icon-ios-folder-open'
+					bgColor: '#a9daa5',
+					icon: 'https://gallery.sleepyocean.cn/resource/img/530cf937ba1707baf193f9e52b820e65'
 				}, {
 					id: 'IMGSERVER',
 					name: '沉洋图床',
+					intro: '自己搭的图床，才放心',
 					url: '/imgServer',
-					bgColor: '#3B4B61',
-					icon: 'icon-ios-image'
+					bgColor: '#848484',
+					icon: 'https://gallery.sleepyocean.cn/resource/img/8b7f9c37adbd78a80c714e851a91f7b3'
 				}, {
 					id: 'POSTALIEN',
 					name: '请求发射器',
+					intro: '微信机器人请求模拟',
 					url: '/postAlien',
-					bgColor: '#3E6070',
-					icon: 'icon-ios-rocket'
+					bgColor: '#4995b9',
+					icon: 'https://gallery.sleepyocean.cn/resource/img/594fc9c60816c544f3450aa7a674e1fd'
 				}
 			]
 		};
@@ -104,41 +119,59 @@ export default {
 
 <style lang='scss'>
     .lab-page {
-        width: 100%;
-		.lp-box {
-			display: flex;
-			flex-wrap: wrap;
-			justify-content: center;
-			align-items: center;
+		padding: 20px 0;
+		text-align: center;
+		@media (min-width: 900px) {
+			.lp-box {
+				width: 840px;
+			}
 		}
-
-        .item {
-            width: 200px;
-            height: 100px;
-            padding: 10px;
-            float: left;
-            display: flex;
-            align-items: center;
-            justify-content: center;
-			background-position: bottom;
-			background-size: cover;
-			background-color: #1b2323;
-            font-size: 24px;
-            user-select: none;
-			color: gainsboro;
-            flex-grow: 1;
-            transition: opacity .2s linear;
-			border: 1px solid white;
-
-            i {
-                font-size: 32px;
-                padding-right: 10px;
-				color: gold;
-            }
-
-            &:hover {
-                opacity: 0.8;
-            }
-        }
+		@media (max-width: 400px) {
+			.lp-box {
+				width: 300px;
+			}
+		}
+		.lp-box {
+			margin: 0 auto;
+			.lp-item-box {
+				display: inline-block;
+				padding: 5px 10px;
+			}
+			.lp-item-container {
+				width: 256px;
+				height: 100px;
+				padding: 10px 80px 10px 20px;
+				border-radius: 4px;
+				float: left;
+				background-repeat: no-repeat;
+				background-position: 90% bottom;
+				background-size: 25%;
+				font-size: 24px;
+				user-select: none;
+				color: white;
+				flex-grow: 1;
+				transition: opacity .2s linear;
+				border: 1px solid white;
+				&:hover {
+					opacity: 0.8;
+				}
+				.lp-ic-title, .lp-ic-intro {
+					text-align: left;
+				}
+				.lp-ic-title {
+					width: 100%;
+					height: 32px;
+					line-height: 32px;
+					font-size: 22px;
+					font-weight: bold;
+				}
+				.lp-ic-intro {
+					width: 100%;
+					height: 36px;
+					line-height: 18px;
+					font-size: 14px;
+				}
+			}
+		}
     }
 </style>
