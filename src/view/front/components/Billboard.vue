89ea8aa4a4bfbe8840c0f92d7561502fb946aeb5
Ge, Houbao <houge@paypal.com>
2020-11-02 13:36:14 +0800

    feature:「公告板组件」增加循环滚动公告功能

--- src/view/front/components/Billboard.vue
+++ src/view/front/components/Billboard.vue
@@ -1,8 +1,15 @@
 <template>
 	<div class="billboard">
 		<div class="b-head"><el-image class="logo-img-style-40" :src="logoUrl"></el-image>公告板</div>
-		<div class="b-title">{{title}}</div>
-		<div class="b-info">{{info}}<el-link :href="link" type="primary">点击围观</el-link></div>
+		<el-carousel class="b-content" indicator-position="outside" direction="vertical">
+			<el-carousel-item v-for="(item, index) in news" :key="index">
+				<div class="b-content-info">
+					<div class="b-title">{{item.title}}</div>
+					<div class="b-info">{{item.info}}<el-link :href="item.link" type="primary">点击围观</el-link></div>
+					<div class="b-date">{{item.date}}</div>
+				</div>
+			</el-carousel-item>
+		</el-carousel>
 	</div>
 </template>
 
@@ -12,9 +19,17 @@ export default {
 	data () {
 		return {
 			logoUrl: 'https://gallery.sleepyocean.cn/resource/img/e36f2f52cdcf31eb0df212755b4cbf57',
-			title: '新功能上线：沉洋影院',
-			info: '沉洋影院已完成首页制作，小洋同学正在加紧实现后端接口中，想尝鲜的小伙伴们可以先围观提提建议哈。',
-			link: '#/theater'
+			news: [ {
+				title: '更新：沉洋实验室页面重设计',
+				info: '沉洋实验室页面已经完成重新设计，丰富的内容展示，告别之前的单调乏味啦。',
+				date: '2020-10-26',
+				link: '#/lab'
+			}, {
+				title: '新功能上线：沉洋影院',
+				info: '沉洋影院已完成首页制作，小洋同学正在加紧实现后端接口中，想尝鲜的小伙伴们可以先围观提提建议哈。',
+				date: '2020-09-21',
+				link: '#/theater'
+			}]
 		};
 	}
 };
@@ -31,6 +46,9 @@ export default {
 	height: 400px;
 	background-color: aliceblue;
 	border-radius: 4px;
+	.b-head, .b-title, .b-info, .b-date {
+		cursor: default;
+	}
 	.b-head {
 		display: flex;
 		align-items: flex-end;
@@ -50,6 +68,19 @@ export default {
 	.b-info {
 		font-size: 14px;
 		text-align: left;
+	}
+	.b-date {
+		height: 30px;
+		line-height: 30px;
+		text-align: right;
+		font-size: 12px;
+		color: #909399;
+	}
+	.b-content {
+		height: 130px;
+	}
+	.b-content-info {
+		border-bottom: 1px dashed #ffd5d5;
 	}
 }
 </style>
